4b6f3c37e0165e7472a834fd7e020dec8f76461c
SuvorkovRN <suvorkovRN@gmail.com>
2019-11-30 23:14:52 +0500

    Second version for check

--- static/main.js
+++ static/main.js
@@ -4,34 +4,26 @@ class Profile {
                     name: { firstName, lastName },
                     password,
                 }) {
-        this.userName = username;
+        this.username = username;
         this.name = { firstName, lastName };
         console.log(this.name);
         this.password = password;
     }
 
     createUser(callback) {
-        console.log(`Creating user ${this.userName}`);
-        let output = {};
-        output.username = this.userName;
-        output.name = this.name;
-        output.password = this.password;
-        return ApiConnector.createUser(output, (err, data) => {
-                console.log(`User ${this.username} is created`);
-                // console.log(data); выводятся данные из коснструктора плюс кошелек, id и т.д. Похоже данные формируюбтся в строке 180-190
-                // console.log(err); выводит ошибку или null
-                callback(err, data);
+        console.log(`Creating user ${this.username}`);
+
+        return ApiConnector.createUser({username: this.username, name: this.name, password: this.password}, (err, data) => {
+            callback(err, data);
+            // console.log(data); выводятся данные из коснструктора плюс кошелек, id и т.д. Похоже данные формируюбтся в строке 180-190
+            // console.log(err); выводит ошибку или null
         }); 
 
     }
 
     authorizationUser(callback) {
-        console.log(`Authorizating user ${this.userName}`);
-        let output = {};
-        output.username = this.userName;
-        output.password = this.password;
-        return ApiConnector.performLogin(output, (err, data) => {
-                console.log(`User ${this.username} is authorized`);
+        console.log(`Authorizating user ${this.username}`);
+        return ApiConnector.performLogin({username: this.username, password: this.password}, (err, data) => {
                 callback(err, data);
         }); 
 
@@ -45,18 +37,18 @@ class Profile {
         });
     }
 
-    covertMoney({inputCurrency, outputCurrency, currencySum}, callback) {
-        console.log(`Converting ${inputCurrency} of ${currencySum} to ${outputCurrency}`);
-        return ApiConnector.convertMoney({ inputCurrency, outputCurrency, currencySum }, (err, data) => {
-            console.log(`Converted to ${outputCurrency} of ${currencySum} to ${this.username}`);
+    convertMoney({fromCurrency, targetCurrency, targetAmount}, callback) {
+        console.log(`Converting ${fromCurrency} to ${targetAmount} ${targetCurrency}`);
+        return ApiConnector.convertMoney({ fromCurrency, targetCurrency, targetAmount }, (err, data) => {
+            console.log(`Converted to coins ${this}`);
             callback(err, data);
         });
     }
 
-    transfer({targetUser, sum}, callback) {
-        console.log(`Transfering ${sum} of Netcoins to ${targetUser}`);
-        return ApiConnector.transferMoney({ targetUser, sum }, (err, data) => {
-            console.log(`${targetUser} has got ${sum} of Netcoins`);
+    transfer({to, amount}, callback) {
+        console.log(`Transfering ${amount} of Netcoins to ${to}`);
+        return ApiConnector.transferMoney({ to, amount }, (err, data) => {
+            console.log(`${to} has got ${amount} of Netcoins`);
             callback(err, data);
         });
     }
@@ -74,33 +66,96 @@ function getCurriencies(callback) {
 }
 
 
-let currienciesRate = getCurriencies((err, data) => {
-        if (err) {
-            console.log(`Error during getting curriencies rate`);
-        } else {
-            console.log(data);
-            return data;
-        } 
-});
+// const currienciesRate = getCurriencies((err, data) => {
+//         if (err) {
+//             console.log(`Error during getting curriencies rate`);
+//         } else {
+//             console.log(data);
+//             return data;
+//         } 
+// });
 
-console.log(currienciesRate);
+
+                        
 
 function main() {
-    const Oleg = new Profile({
+    
+    // getCurriencies((err, data) => {
+    //     if (err) {
+    //         console.log(`Error during getting curriencies rate`);
+    //     } else {
+    //         console.log(data);
+    //         return data;
+    //     } 
+    // });
+    // const currienciesRate = data[99];
+    
+    // console.log(currienciesRate);
+    
+    const Oleg2 = new Profile({
                     username: 'Oleg',
                     name: { firstName: 'Oleg', lastName: 'Olegov' },
                     password: 'olegspass',
                 });
-    console.log(Oleg);
-//     // сначала создаем и авторизуем пользователя
 
-//     // после того, как мы авторизовали пользователя, добавляем ему денег в кошелек
-    Oleg.createUser((err, data) => {
+    const Ivan = new Profile({
+        username: 'Ivan',
+        name: { firstName: 'Ivan', lastName: 'Ivanov' },
+        password: 'ivanspass',
+    });
+
+    Oleg2.createUser((err, data) => {
         if (err) {
-          console.log(`Error during creating ${Oleg.username}`);
-        } else {
-          console.log(`User ${Oleg.username} successfully created`);
-        }
+            console.log(`Error during creating ${Oleg2.username}`);
+            } else {
+                console.log(`User ${Oleg2.username} successfully created`);
+                Oleg2.authorizationUser((err, data) => {
+                    if (err) {
+                    console.log(`Error during authorizating ${Oleg2.username}`);
+                    }else {
+                        console.log(`User ${Oleg2.username} is authorized`);
+                        Oleg2.addMoney({ currency: 'RUB', amount: 10000 }, (err, data) => {
+                            if (err) {
+                                console.log(`Error during adding money to ${Oleg2.username}`);
+                            } else {
+                                console.log(`Added 10000 RUB to Oleg`);
+                                getCurriencies((err, data) => {
+                                    if (err) {
+                                       console.log(`Error during getting curriencies rate`);
+                                    } else {
+                                        const currienciesRate = data[99];
+                                        const targetCurrencySum = currienciesRate.RUB_NETCOIN * 10000;
+                                        Oleg2.convertMoney({fromCurrency: 'RUB', targetCurrency: 'NETCOIN', targetAmount: targetCurrencySum}, (err, data) => {
+                                            if (err) {
+                                                console.log(`Error during converting money`);                                     
+                                            } else {
+                                                Ivan.createUser((err, data) => {
+                                                    if (err) {
+                                                        console.log(`Error during creating ${Ivan.username}`);
+                                                    } else {
+                                                        Ivan.transfer({to: 'Ivan', amount: targetCurrencySum}, (err, data) => {
+                                                            if (err) {
+                                                                console.log(`Error transfering Netcoins to ${Ivan.username}`);
+                                                            } else {
+                                                                console.log(`${Ivan.username} has got ${targetCurrencySum} of NETCOINS`);
+                                                            }
+                                                        
+                                                        
+                                                        })
+                                                        
+                                                        
+                                                    }
+                                                })
+                                            }
+                                                                                        
+                                        })
+                                    } 
+                                });
+                            }
+                        })
+                    }
+                })
+            }
     })
 }
 
